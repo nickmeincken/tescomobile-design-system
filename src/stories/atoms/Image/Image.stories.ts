20a2f9f2afa58a0451aa36d984be78abe67fadb4
nickmeincken <nicholas.meincken@tesco.com>
2025-12-24 14:42:43 +0000

    fix logo

--- src/stories/atoms/Image/Image.stories.ts
+++ src/stories/atoms/Image/Image.stories.ts
@@ -23,7 +23,7 @@ export const LogoImage: Story = {
   },
   render: () => html`
     <a href="https://www.tescomobile.com/" class="logo" title="Tesco Mobile" aria-label="Store Logo">
-      <svg><use xlink:href="/svg/sprite.svg#tescomobile-logo"></use></svg>
+      <svg><use xlink:href="./svg/sprite.svg#tescomobile-logo"></use></svg>
       <span>Tesco Mobile</span>
     </a>`,
 };
